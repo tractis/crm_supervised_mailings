63f0c8e48d5b8fdab1116f2319a28e69099df206
jose-gordo <jose.gordo@tractis.com>
2010-03-03 11:11:25 +0100

    fix associations

--- app/models/account_mailing_mail_associations.rb
+++ app/models/account_mailing_mail_associations.rb
@@ -2,7 +2,7 @@ module AccountMailingMailAssociations
   
   def self.included(base)
     base.class_eval do
-      has_many :mailings_mails, :dependent => :destroy, :as => :mailable
+      has_many :mailing_mails, :dependent => :destroy, :as => :mailable
       has_many :mailings, :through => :mailing_mails
     end
   end
