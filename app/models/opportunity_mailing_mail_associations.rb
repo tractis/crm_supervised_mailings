9a26733a7cccca6de6105c8297feea8a6b7763eb
jose.gordo <jose.gordo@pepele-desktop.(none)>
2010-05-03 09:43:15 +0200

    experimental opportunity and filters feature, use v1.0 tag for stable ffc code

---
+++ app/models/opportunity_mailing_mail_associations.rb
@@ -0,0 +1,10 @@
+module OpportunityMailingMailAssociations
+  
+  def self.included(base)
+    base.class_eval do
+      has_many :mailing_mails, :dependent => :destroy, :as => :mailable
+      has_many :mailings, :through => :mailing_mails
+    end
+  end
+
+end
